f5f254b6b9df36c21b43d0bc96b5332cbdde41f6
pinocchio <81025400+Pinocchio009@users.noreply.github.com>
2022-12-03 06:14:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const customer = prompt('what is your name')
+
+//reply ok
+const goods = prompt('This are the goods available now (either fish, meat, pepper):')
+
+// input the goods you want
+const cart = prompt('what do you want to buy')
+
+let result;
+
+if (cart == 'fish') {
+  result = `hello ${customer}, the ${cart} you requested is sold for 2naira, and has been added to your cart`
+}
+else if (cart == 'meat') {
+  result = `hello ${customer}, the ${cart} you requested for is sold for 10 naira, and has been added to your cart`
+}
+else if (cart == 'pepper') {
+  result = `hello ${customer}, the ${cart} you requested for is sold for 1 naira, and has been added to your cart`
+}
+else if (cart != goods) {
+  result = ' we are out of stock'
+}
+
+console.log(` ${result}`)
