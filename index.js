e31d1b752a257add220dead81f6384d51c957740
pinocchio <81025400+Pinocchio009@users.noreply.github.com>
2022-12-04 11:16:21 +0000

    i dont know

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const cart = prompt('what do you want to buy')
 
 let result;
 
+while( cart == 'fish', 'meat', 'pepper'){
+  console.log(`You just purchsed ${cart}`)
+  return result
+}
+
 if (cart == 'fish') {
   result = `hello ${customer}, the ${cart} you requested is sold for 2naira, and has been added to your cart`
 }
